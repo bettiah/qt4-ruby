867412706577ce8dacca7cc544ebb66014b32a47
bettiah <bettiah@47233f48-bd19-0410-af3c-33b5cb0f13b4>
2006-11-29 17:24:21 +0000

    small example updates

--- trunk/1-hello-world.rb
+++ trunk/1-hello-world.rb
@@ -1,21 +1,15 @@
 require 'gui'
 include Gui
 
-class Symbol
-   def to_proc
-      proc { |obj, *args| obj.send(self, *args) }
-   end
-end
-
 class Btn < PushButton
    def on_click
       message_box(:info, sender.getp(:text))
    end
 
    def initialize()
-	   super
-	   self.setp(:text=>"Click", :'minimum-width'=>150, :'minimum-height'=>30)
- 	   self.connect :clicked, :on_click
+	   super(:text=>"Click", :'minimum-width'=>150, :'minimum-height'=>30) { |bt|
+ 	   		bt.connect :clicked, :on_click
+	   }
    end
 end
 
